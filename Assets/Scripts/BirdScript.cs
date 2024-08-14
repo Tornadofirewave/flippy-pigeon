e5aedcf9b343e0df3720d8c5e7ac4ad7ccf9337f
Tornadofirewave <142632652+Tornadofirewave@users.noreply.github.com>
2024-08-14 00:17:39 -0700

    minor changes

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -8,19 +8,17 @@ public class foxscript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public float flapStrength;
     public LogicScript logic;
-    public bool birdIsAlive = true;
+    public static bool birdIsAlive = true;
 
-    // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         birdIsAlive = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
